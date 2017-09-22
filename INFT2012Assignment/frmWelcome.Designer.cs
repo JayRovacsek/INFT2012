e5c014eab7183bf0d42bf7b867a7fa9d4ae8d6e6
RandomGeneric <jayrovacsek@gmail.com>
2017-09-22 11:11:12 +0100

    Update

--- INFT2012Assignment/frmWelcome.Designer.cs
+++ INFT2012Assignment/frmWelcome.Designer.cs
@@ -28,11 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmWelcome));
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.lblRules = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.Location = new System.Drawing.Point(12, 9);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(390, 46);
+            this.lblInfo.TabIndex = 0;
+            this.lblInfo.Text = "Welcome to PlusMoins, A game of frustration, and dice. In this game you will roll" +
+    " dice in order to achieve a desired score against either an AI opponent or a fri" +
+    "end. The rules include;";
+            // 
+            // lblRules
+            // 
+            this.lblRules.Location = new System.Drawing.Point(12, 55);
+            this.lblRules.Name = "lblRules";
+            this.lblRules.Size = new System.Drawing.Size(483, 294);
+            this.lblRules.TabIndex = 1;
+            this.lblRules.Text = resources.GetString("lblRules.Text");
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 352);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(488, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "I got it, let\'s start suffering!";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // frmWelcome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "frmWelcome";
+            this.ClientSize = new System.Drawing.Size(512, 386);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblRules);
+            this.Controls.Add(this.lblInfo);
+            this.Name = "frmWelcome";
+            this.Text = "Welcome to PlusMoins!";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Label lblRules;
+        private System.Windows.Forms.Button btnStart;
     }
 }
