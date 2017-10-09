60fae1ebfaf92af7bf0021e0a7f8fb7717dea5ff
RandomGeneric <jayrovacsek@gmail.com>
2017-10-09 19:08:42 +1100

    Update to include AI

---
+++ INFT2012Assignment/AI.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INFT2012Assignment
+{
+    class AI
+    {
+        // LOOK AT PERSON.CS IN WEEK 10 DEMOs
+    }
+}
